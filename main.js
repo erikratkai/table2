6068ef5d9296ad04788e57d90f34f0976481a867
Ratkai Erik <ratkai.erik@BOLYAI.LOCAL>
2024-10-11 14:36:22 +0200

    add person function

--- main.js
+++ main.js
@@ -62,65 +62,11 @@ th_married.innerHTML = 'Házastárs';
 const tbody = document.createElement('tbody');
 table.appendChild(tbody);
 
-for(const pers of array){
-    const tbody_tr = document.createElement('tr');
-    tbody.appendChild(tbody_tr);
-    
-  
 
-    const tbody_td_lastname = document.createElement('td');
-    tbody_tr.appendChild(tbody_td_lastname);
-   
-    tbody_td_lastname.innerHTML = pers.lastname;
- 
-    const tbody_td_firstname = document.createElement('td');
-    tbody_tr.appendChild(tbody_td_firstname);
-   
-    tbody_td_firstname.innerHTML = pers.firstname1;
-   
-
-
-    
-    if(pers.firstname2 === undefined){
-        tbody_td_firstname.colSpan = 2
-    }
-    else{
-        const tbody_td_firstname = document.createElement('td');
-        tbody_tr.appendChild(tbody_td_firstname);
-       
-        tbody_td_firstname.innerHTML = pers.firstname2;
-    }
-
-    const td_pet = document.createElement('td');
-    tbody_tr.appendChild(td_pet);
-    td_pet.innerHTML = pers.pet;
-  
-
-    const td_married = document.createElement('td');
-    tbody_tr.appendChild(td_married);
-    td_married.innerHTML = pers.married;
-    tbody_tr.addEventListener('click', function(e){
-
-       const isSelected = tbody.querySelector('.selected');
-        if( isSelected != undefined){
-            isSelected.classList.remove('selected');
-       }
-
-       e.currentTarget.classList.add('selected');
-       
-    })
-
-    if(pers.married === true){
-        td_married.innerHTML = 'Igen';
-    }
-    else{
-        td_married.innerHTML = 'Nem';
-    }
- 
-}
 
 const form = document.getElementById('form');
 form.addEventListener('submit',function(e){
+    e.preventDefault();
     const lastname = document.getElementById('lastname');
     const firstname1 = document.getElementById('firstname1');
     const firstname2 = document.getElementById('firstname2');
@@ -132,4 +78,82 @@ form.addEventListener('submit',function(e){
     const lastnameValue = lastname.value;
     const marriedValue = married.checked;
     const petValue = pet.value;
+
+    if(pers.firstname2 === undefined){
+        tbody_td_firstname.colSpan = 2
+    }
+
+    const newperson ={
+        firstname1: firstname1Value,
+        firstname2: firstname2Value,
+        lastname: lastnameValue,
+        married: marriedValue,
+        pet: petValue
+    }
+
+    array.push(newperson);
+    console.log(array);
+
+    rendertable();
 })
+rendertable();
+
+function rendertable(){
+    tbody.innerHTML="";
+    for(const pers of array){
+        const tbody_tr = document.createElement('tr');
+        tbody.appendChild(tbody_tr);
+        
+      
+    
+        const tbody_td_lastname = document.createElement('td');
+        tbody_tr.appendChild(tbody_td_lastname);
+       
+        tbody_td_lastname.innerHTML = pers.lastname;
+     
+        const tbody_td_firstname = document.createElement('td');
+        tbody_tr.appendChild(tbody_td_firstname);
+       
+        tbody_td_firstname.innerHTML = pers.firstname1;
+       
+    
+    
+        
+        if(pers.firstname2 === undefined){
+            tbody_td_firstname.colSpan = 2
+        }
+        else{
+            const tbody_td_firstname = document.createElement('td');
+            tbody_tr.appendChild(tbody_td_firstname);
+           
+            tbody_td_firstname.innerHTML = pers.firstname2;
+        }
+    
+        const td_pet = document.createElement('td');
+        tbody_tr.appendChild(td_pet);
+        td_pet.innerHTML = pers.pet;
+      
+    
+        const td_married = document.createElement('td');
+        tbody_tr.appendChild(td_married);
+        td_married.innerHTML = pers.married;
+        tbody_tr.addEventListener('click', function(e){
+    
+           const isSelected = tbody.querySelector('.selected');
+            if( isSelected != undefined){
+                isSelected.classList.remove('selected');
+           }
+    
+           e.currentTarget.classList.add('selected');
+           
+        })
+    
+        if(pers.married === true){
+            td_married.innerHTML = 'Igen';
+        }
+        else{
+            td_married.innerHTML = 'Nem';
+        }
+     
+    }
+}
